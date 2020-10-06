709121ca2e5583264e32164b3c46bb95d12f539d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-10-06 22:36:22 +0530

    Performance and UI improvements, and documentation
    code for walking is now properly documented.
    reverse_mapping is now read from json, which is faster.
    Movie frequencies are now calculated as weighted averages.
    Better tkinter UI

--- random_walker.py
+++ random_walker.py
@@ -1,11 +1,10 @@
 from multiprocessing import Pool
 import random
-import time
+import json
 import graph_tool.all as gt
 import numpy as np
-# import matplotlib.cm as cm
-# import pandas as pd
-
+import pandas as pd
+from fuzzywuzzy import process
 
 """
 # creating random graph
@@ -35,54 +34,70 @@ def create_edges():
     print(f"added edges{time.time()}")
 """
 
-# create_edges()
-# g.save("random.gt")
-# gt.random_rewire(g, model='erdos', n_iter=nedges)
-# print(f"did erdos renyi{time.time()}")
-# n = 10   # number of walks
+# load the graph
 g = gt.load_graph("random.gt")
-# movies = pd.read_csv("ml-25m/movies.csv")
+# load the titles
+titles = pd.read_csv("ml-25m/movies.csv")['title'].to_numpy()
+# load the reverse mapping from title to vertex index. This is faster than calculating from title
+with open('reverse_mapping.json') as infile:
+    reverse_mapping = json.load(infile)
 
-# names = g.new_vertex_property("string", vals=movies['title'].to_numpy())
-# g.vertex_properties["name"] = names
-# g.save("random.gt")
+# how many iterations to walk for
+walk_length = 100000
 
 
-def walk(vertex):
+def walk(vertex: int):
+    """
+    Does a random walk from starting vertex
+    """
     reset_probability = 0.2
     # creating the vertex property map for frequency
     fvp = g.new_vertex_property('int', val=0)
+    # getting the corresponding vertex
     vert = g.vertex(vertex)
 
-    for _ in range(100000):
+    for _ in range(walk_length):
+        # increment the frequency of this vertex
         fvp[vert] += 1
-        # with reset_probability we go back to origin vertex
         if random.uniform(0, 1) < reset_probability:
+            # with reset_probability we go back to origin vertex
             vert = g.vertex(vertex)
         else:
             # otherwise just go to a random neighbour
             vert = random.choice(g.get_out_neighbors(vert))
+    # returns the frequency map as a np.ndarray
     return fvp.a
 
 
-def run_walk(n):
+def names_to_indices(names: list):
+    # this is just QoL, allows passing a single string instead of a list
+    if not isinstance(names, list):
+        names = [names]
+    # make sure the names are all strings
+    assert all(isinstance(name, str) for name in names)
+    # get the corresponding best match
+    return [process.extractOne(name, titles) for name in names]
 
-    with Pool(n) as p:
+
+def run_walk(movies, weights):
+    # value sanity checks
+    assert len(movies) == len(weights)
+    assert all(isinstance(w, float) for w in weights)
+
+    # get the vertex indices for the movie names
+    indices = [reverse_mapping[n[0]] for n in names_to_indices(movies)]
+
+    with Pool(16) as pool:
         # walk from node 1 n times
-        print(f"start walks {time.time()}")
-        maps = p.map(walk, [1 for i in range(n)])
+        maps = pool.map(walk, indices)
         # add the resultant frequency arrays into one
-        print(f"after walks{time.time()}")
-        result = np.ndarray(g.num_vertices(), dtype=int)
-        for vmap in maps:
-            result += vmap
+        result = np.ndarray(g.num_vertices(), dtype=float)
 
-        # set the frequency into a map
-        # freq = g.new_vertex_property('int', vals=result)
-        # labels = g.new_vertex_property('string', vals=list(range(100)))
+        # add the correponsing freuquencies, multiplied by respective weights
+        for i in range(len(movies)):
+            result += maps[i] * weights[i]
 
-        # drawing
-        # pos = gt.radial_tree_layout(g, 1)
-        # gt.graph_draw(g, pos, vertex_fill_color=freq, vcmap=cm.viridis, vprops={'text': labels})
+        # just fancy stuff necessary to get 5 maximum indices from results, and display their
+        # corresponding movie names
         idx = np.argpartition(result, -5)[-5:]
-        print([g.vertex_properties["name"][x] for x in idx[np.argsort((-result)[idx])]])
+        return [g.vertex_properties["name"][x] for x in idx[np.argsort((-result)[idx])]]
