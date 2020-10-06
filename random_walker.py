df178ea6930ce6432a9d115adac80f8f7af30ec1
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-10-06 19:30:00 +0530

    Random walker improvements
    Random walker now reads graph from file. Currently, this is just a random
    movie network for testing. Additionally, it prints the 5 most important movies
    Also, there's a .gitignore now

--- random_walker.py
+++ random_walker.py
@@ -1,18 +1,51 @@
 from multiprocessing import Pool
 import random
+import time
 import graph_tool.all as gt
 import numpy as np
-import matplotlib.cm as cm
+# import matplotlib.cm as cm
+# import pandas as pd
 
+
+"""
 # creating random graph
-g = gt.Graph(directed=False)
-g.add_vertex(100)
-for it in range(1000):
-    a, b = random.sample(range(100), 2)
-    g.add_edge(a, b)
-gt.random_rewire(g, model='erdos', n_iter=1000)
+print(f"initial{time.time()}")
+movies = pd.read_csv('ml-25m/movies.csv')
+print(f"read{time.time()}")
+print(len(movies))
 
+g = gt.Graph(directed=False)
+g.add_vertex(len(movies) // 2)
+print(f"made graph{time.time()}")
+nedges = int(len(movies)**2 * 0.01 * 0.25)
+
+print(f"nedges {nedges}")
+
+
+def make_edge(_):
+    return random.sample(range(len(movies)), 2)
+
+
+def create_edges():
+    print("here")
+    edges = []
+    with Pool(16) as p:
+        edges = p.map(make_edge, range(nedges))
+    g.add_edge_list(edges)
+    print(f"added edges{time.time()}")
+"""
+
+# create_edges()
+# g.save("random.gt")
+# gt.random_rewire(g, model='erdos', n_iter=nedges)
+# print(f"did erdos renyi{time.time()}")
 # n = 10   # number of walks
+g = gt.load_graph("random.gt")
+# movies = pd.read_csv("ml-25m/movies.csv")
+
+# names = g.new_vertex_property("string", vals=movies['title'].to_numpy())
+# g.vertex_properties["name"] = names
+# g.save("random.gt")
 
 
 def walk(vertex):
@@ -33,18 +66,23 @@ def walk(vertex):
 
 
 def run_walk(n):
+
     with Pool(n) as p:
         # walk from node 1 n times
+        print(f"start walks {time.time()}")
         maps = p.map(walk, [1 for i in range(n)])
         # add the resultant frequency arrays into one
-        result = np.ndarray(100, dtype=int)
+        print(f"after walks{time.time()}")
+        result = np.ndarray(g.num_vertices(), dtype=int)
         for vmap in maps:
             result += vmap
 
         # set the frequency into a map
-        freq = g.new_vertex_property('int', vals=result)
-        labels = g.new_vertex_property('string', vals=list(range(100)))
+        # freq = g.new_vertex_property('int', vals=result)
+        # labels = g.new_vertex_property('string', vals=list(range(100)))
 
         # drawing
-        pos = gt.radial_tree_layout(g, 1)
-        gt.graph_draw(g, pos, vertex_fill_color=freq, vcmap=cm.viridis, vprops={'text': labels})
+        # pos = gt.radial_tree_layout(g, 1)
+        # gt.graph_draw(g, pos, vertex_fill_color=freq, vcmap=cm.viridis, vprops={'text': labels})
+        idx = np.argpartition(result, -5)[-5:]
+        print([g.vertex_properties["name"][x] for x in idx[np.argsort((-result)[idx])]])
