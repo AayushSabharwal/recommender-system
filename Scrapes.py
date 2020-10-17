1db5952f44025435e10ee629308ec17698ad84e8
Harsh Priyadarshi <52668222+harshest2019@users.noreply.github.com>
2020-10-18 00:02:05 +0530

    Added IMDB ID, 25M ID
    It will now add imdb id and 25m id with the scraped data.

--- Scrapes.py
+++ Scrapes.py
@@ -6,16 +6,19 @@ data=reader(data)
 data=list(data)
 imdb_id=[]
 for ids in data:
-    imdb_id.append(ids[1])
+    imdb_id.append((ids[1],ids[2]))
 a=int(input("Enter the starting index of the movie (this index is included) "))
 b=int(input("Enter the ending index of the movie (this index is excluded) "))
 id_format="tt{}"
 # 0th index is not the id, omit that 
-final={'Title':[],'Genre':[],'Tags':[],'Director':[],'Cast':[],'Language':[],'Rating':[]}
+final={'IMDB':[],'ID':[],'Title':[],'Genre':[],'Tags':[],'Director':[],'Cast':[],'Language':[],'Rating':[]}
+i=a
 for ids in imdb_id[a:b]:
     try:
-        test=id_format.format(ids)
+        test=id_format.format(ids[0])
         titles,genres,keywords,c,d,e,f=scraper.credentials(test)
+        final['IMDB'].append(ids[0])
+        final['ID'].append(ids[1])
         final['Title'].append(titles)
         final['Genre'].append(genres)
         final['Tags'].append(keywords)
@@ -23,9 +26,10 @@ for ids in imdb_id[a:b]:
         final['Cast'].append(d)
         final['Language'].append(e)
         final['Rating'].append(f)
-        print("Scraped out",titles)
+        print(i, "Scraped out",titles)
     except:
         print(test,"is throwing an error")
+    i+=1
 pd1=pd.DataFrame(final)
 compression_opts = dict(method='zip',
                         archive_name='out.csv') 
