de7c6c832e6b93d62a430e8d4275d140a4baf9d1
harshest2019 <harshp19012002@gmail.com>
2020-11-16 03:23:52 +0530

    Update User_id.py
    It now takes list as an input.

--- user_id.py
+++ user_id.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
 ''' 
-Takes as input a movieID and exports list of USER IDs
+Takes as input a list of movieIDs and exports list of USER IDs
 '''
 # Tutorial
 '''
 import user_id
-c=user_id.subset(180079)
+c=user_id.subset([180079,180075])
 '''
+
 
 def subset(movieID):
     count=pd.read_csv('Len_movies.csv',index_col=False)
     genre=pd.read_csv('Movie_data_cleaned.csv')
     genre=genre.set_index('movieId')
-    genres=genre._get_value(movieID,'genres').split("/")
+    genres=[]
+    for x in movieID:
+        genress=genre._get_value(x,'genres').split("/")
+        genres.extend(genress)
     normalized=pd.read_csv("Scores_Normalized.csv",index_col=False)
     subdataframe=normalized[genres]
     subdataframe=subdataframe.copy()
