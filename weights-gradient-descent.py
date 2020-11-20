7626196382c829805c5c7b7b75be92ac69aaf0cc
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2020-11-20 10:46:59 +0530

    weights gradient descent

--- weights-gradient-descent.py
+++ weights-gradient-descent.py
@@ -3,29 +3,31 @@ from scipy import optimize as op
 import numba
 
 similarity_files = ['all_similarities/director-similarity', 'all_similarities/genre-similarity', 'all_similarities/rating-similarity', 'all_similarities/tags-similarity']
-with np.load('all_similarities/user_similarity.npz', 'r') as npf:
+with np.load('all_similarities/user-similarity.npz', 'r') as npf:
     usim = npf['arr_0']
+adj_mat = np.zeros((23843, 23843), dtype='f4')
 
-@numba.jit(forceobj=True, parallel=True, fastmath=True)
-def calc_adj_mat(wts):
-    adj_mat_t = np.zeros((23843, 23843), dtype='<f2')
-    for param, weight in zip(similarity_files, wts):
-        npf = np.memmap(param+'.npy', mode='r', shape=adj_mat_t.shape, dtype='<f2')
-        adj_mat_t += npf * weight
-    return adj_mat_t
-
-@numba.jit(forceobj=True, cache=True, parallel=True, fastmath=True)
-def weightfn(x):
-    print(x)
-    adj_mat = calc_adj_mat(x)
+def weightfn(x, *args):
+    print(x, x/x.sum(), end=' ')
+    adj_mat = args[0]
+    adj_mat -= adj_mat
+    for param, weight in zip(similarity_files, x):
+        with np.load(param+'.npz', 'r') as npf:
+        #npf = np.memmap(param+'.npy', mode='r', shape=adj_mat.shape, dtype='f4')
+            adj_mat += npf['arr_0'] * weight
+    adj_mat /= x.sum()
     adj_mat -= usim
     np.abs(adj_mat, out=adj_mat)
-    return adj_mat.sum(dtype='f4')
+    ret = adj_mat.sum()
+    print(ret)
+    return ret
+
 
 print(f'Enter weights {len(similarity_files)} space separated float values')
 inp = [float(x) for x in input().split(' ')]
-weights = np.array(inp, dtype='f2')
-res = op.minimize(weightfn, weights, method='BFGS', options={'disp': True})
+weights = np.array(inp, dtype='f4')
+
+res = op.minimize(weightfn, weights, method='BFGS', options={'disp': True}, args=(adj_mat, ))
 
 print('final fn value', res.fun)
 print('final x value', res.x)
